9d8204707d81da72bc1012a7399bb0eb64e6ef7c
antonbaider <email.b.anton@gmail.com>
2024-01-18 18:01:45 +0100

    tasks done

--- src/main/java/com/softserve/itacademy/controller/HomeController.java
+++ src/main/java/com/softserve/itacademy/controller/HomeController.java
@@ -1,14 +1,24 @@
 package com.softserve.itacademy.controller;
 
+import com.softserve.itacademy.model.User;
+import com.softserve.itacademy.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
+@AllArgsConstructor
 public class HomeController {
-    //ToDo
 
-//    @GetMapping({"/", "home"})
-//    public String home(//add needed parameters) {
-//        //ToDo
-//        return "";
-//    }
+    private final UserService userService;
+
+    @GetMapping({"/", "home"})
+    public String home(Model model) {
+        List<User> users = userService.getAll();
+        model.addAttribute("users", users);
+        return "home";
+    }
 }
--- src/main/java/com/softserve/itacademy/controller/UserController.java
+++ src/main/java/com/softserve/itacademy/controller/UserController.java
@@ -1,15 +1,29 @@
 package com.softserve.itacademy.controller;
 
+import com.softserve.itacademy.model.Role;
+import com.softserve.itacademy.model.User;
+import com.softserve.itacademy.repository.UserRepository;
+import com.softserve.itacademy.service.RoleService;
+import com.softserve.itacademy.service.UserService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
-
+   private final UserService userService;
     //add needed fields
+    private final RoleService roleService;
 
-//    @GetMapping("/create")
+    public UserController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    //    @GetMapping("/create")
 //    public String create(//add needed parameters) {
 //        //ToDo
 //        return " ";
@@ -21,17 +35,39 @@ public class UserController {
 //        return " ";
 //    }
 //
-//    @GetMapping("/{id}/read")
-//    public String read(//add needed parameters) {
-//        //ToDo
-//        return " ";
-//    }
-//
-//    @GetMapping("/{id}/update")
-//    public String update(//add needed parameters) {
-//        //ToDo
-//        return " ";
-//    }
+@GetMapping("/{id}/read")
+public String read(@PathVariable("id") long id,
+                   Model model) {
+
+    User user = userService.readById(id);
+
+    model.addAttribute("user", user);
+
+    return "user-info";
+}
+
+    @GetMapping("/{id}/update")
+    public String update(@PathVariable("id") long id,
+                         Model model) {
+
+        User user = userService.readById(id);
+        model.addAttribute("user", user);
+
+        List<Role> allRoles = roleService.getAll();
+        model.addAttribute("roles", allRoles);
+
+        return "update-user";
+    }
+    @PostMapping("/{id}/update")
+    public String updateUser(@PathVariable("id") long id,
+                             @ModelAttribute("user") User updatedUser) {
+
+        // Validate and update the user data
+        userService.update(updatedUser);
+
+        // Redirect to the Home page after updating
+        return "redirect:/home";
+    }
 //
 //
 //    @GetMapping("/{id}/delete")
