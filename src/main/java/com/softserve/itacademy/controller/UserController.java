d81381d9ac708e172ec89f6751e9eef40710116d
antonbaider <email.b.anton@gmail.com>
2024-01-18 18:06:07 +0100

    fix code

--- src/main/java/com/softserve/itacademy/controller/UserController.java
+++ src/main/java/com/softserve/itacademy/controller/UserController.java
@@ -2,7 +2,6 @@ package com.softserve.itacademy.controller;
 
 import com.softserve.itacademy.model.Role;
 import com.softserve.itacademy.model.User;
-import com.softserve.itacademy.repository.UserRepository;
 import com.softserve.itacademy.service.RoleService;
 import com.softserve.itacademy.service.UserService;
 import org.springframework.stereotype.Controller;
@@ -14,7 +13,7 @@ import java.util.List;
 @Controller
 @RequestMapping("/users")
 public class UserController {
-   private final UserService userService;
+    private final UserService userService;
     //add needed fields
     private final RoleService roleService;
 
@@ -35,37 +34,26 @@ public class UserController {
 //        return " ";
 //    }
 //
-@GetMapping("/{id}/read")
-public String read(@PathVariable("id") long id,
-                   Model model) {
-
-    User user = userService.readById(id);
-
-    model.addAttribute("user", user);
-
-    return "user-info";
-}
+//    @GetMapping("/{id}/read")
+//    public String read(//add needed parameters) {
+//        //ToDo
+//        return " ";
+//    }
 
     @GetMapping("/{id}/update")
     public String update(@PathVariable("id") long id,
                          Model model) {
-
         User user = userService.readById(id);
         model.addAttribute("user", user);
-
         List<Role> allRoles = roleService.getAll();
         model.addAttribute("roles", allRoles);
-
         return "update-user";
     }
+
     @PostMapping("/{id}/update")
     public String updateUser(@PathVariable("id") long id,
                              @ModelAttribute("user") User updatedUser) {
-
-        // Validate and update the user data
         userService.update(updatedUser);
-
-        // Redirect to the Home page after updating
         return "redirect:/home";
     }
 //
